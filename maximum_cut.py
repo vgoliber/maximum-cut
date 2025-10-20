67183557c2be2b7a593a62470f1a138e94315d86
Victoria Goliber <vgoliber@dwavesys.com>
2025-10-20 15:18:06 -0700

    Update to symbolic

--- maximum_cut.py
+++ maximum_cut.py
@@ -15,8 +15,8 @@
 # ------ Import necessary packages ----
 from collections import defaultdict
 
-from dwave.system.samplers import DWaveSampler
-from dwave.system.composites import EmbeddingComposite
+from dimod import BinaryQuadraticModel, Binary
+from dwave.system import DWaveSampler, EmbeddingComposite
 import networkx as nx
 
 import matplotlib
@@ -33,14 +33,15 @@ G.add_edges_from([(1,2),(1,3),(2,4),(3,4),(3,5),(4,5)])
 
 # ------- Set up our QUBO dictionary -------
 
-# Initialize our Q matrix
-Q = defaultdict(int)
+# Initialize BQM
+bqm = BinaryQuadraticModel('BINARY')
 
-# Update Q matrix for every edge in the graph
+# Define variables
+x = {i: Binary(i) for i in G.nodes}
+
+# Update BQM for every edge in the graph
 for i, j in G.edges:
-    Q[(i,i)]+= -1
-    Q[(j,j)]+= -1
-    Q[(i,j)]+= 2
+    bqm += 2*x[i]*x[j] - x[i] - x[j]
 
 # ------- Run our QUBO on the QPU -------
 # Set up QPU parameters
@@ -49,7 +50,7 @@ numruns = 10
 
 # Run the QUBO on the solver from your config file
 sampler = EmbeddingComposite(DWaveSampler())
-response = sampler.sample_qubo(Q,
+response = sampler.sample(bqm,
                                chain_strength=chainstrength,
                                num_reads=numruns,
                                label='Example - Maximum Cut')
